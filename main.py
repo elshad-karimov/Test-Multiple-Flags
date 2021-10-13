febc9dc7e2b593606ff4632e59486d94a1588d86
Elshad Karimov <32020767+elshad-karimov@users.noreply.github.com>
2021-10-13 09:59:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Different ways to test multiple flags at 
+# once in Python
+a, b, c = 0, 1, 0
+
+if a == 1 or b == 1 or c == 1:
+  print('passed')
+
+if 1 in (a, b, c):
+  print('passed')
+
+# These only test for truthiness:
+if a or b or c:
+  print('passed')
+
+if any((a, b, c)):
+  print('passed')
+
+
+  
